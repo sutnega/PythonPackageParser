8339ead5c589b103b500ef492926e37d07ecec4a
sutnega <104433317+sutnega@users.noreply.github.com>
2023-07-12 19:53:27 +0300

    Переменные управления, сохранение в один файл, полный парсинг

--- packageParser.py
+++ packageParser.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 # https://www.youtube.com/watch?v=EF9UNlB05Rk&list=PL6plRXMq5RADYaw4Xo111smBcEPNMhdHf&index=3
 # по другому гайду    https://idatica.com/blog/parsing-saytov-na-python-rukovodstvo-dlya-novichkov/  https://pishuverno.ru/kak-napisat-parser-dlya-sajta-na-python/
 # Define URL
+PARSVAR = 2  # 1 сохранять сферы в отдельные файлы     2 сохранить все в один файл
+PARSNUM = 1  # 1 спарсить первые PARSCOUNT сфер        2 спарсить все
+PARSCOUNT = 3
 
 urlMain = "https://tara.unipack.ru"  # ссылка на отрасль
 requests.get(urlMain)
@@ -25,9 +28,9 @@ for elem in MainFilteredParsRes:
             SphereLink1.append(link_url)
 print("Собран массив сфер")
 
-#SphereUrl = "https:" + SphereLink1[2]
 
-def parsSphere(ParsUrl):
+# 1 сохранять в отдельные файлы  2 выдать DataFrame pandas
+def parsSphere(ParsUrl, ParsMode):
     SphereUrl = "https:" + ParsUrl
     requests.get(SphereUrl)
     pages = requests.get(SphereUrl)
@@ -153,13 +156,45 @@ def parsSphere(ParsUrl):
          'Телефон': ArrPhone1,
          'Контакты': CompanyPlaceMass, 'Контактное лицо': ArrContactPerson,
          'Описание': CompanyDescriptionMass})  # загрузка в эксель с помощью panda
-    parsBD.to_excel(
-        './' + 'ParsResult_'+ SphereUrl.replace("https://tara.unipack.ru/", "").replace("russia-", "") +".xlsx" )
 
-    return 'Победа над ' + SphereUrl.replace("https://tara.unipack.ru/", "").replace("russia-", "")
+    if ParsMode == 1:
+        parsBD.to_excel(
+            './' + 'ParsResult_' + SphereUrl.replace(urlMain + "/", "").replace("russia-", "") + ".xlsx")
 
-#len(SphereLink1)
-for i in range(1):
-    ParsUrl = SphereLink1[i]
-    a = parsSphere(ParsUrl)
-    print(a)
+        return 'Победа над ' + SphereUrl.replace(urlMain + "/", "").replace("russia-", "")
+    else:
+        if ParsMode == 2: return parsBD
+
+
+if PARSVAR == 2:
+    writer = pd.ExcelWriter('./FullBD.xlsx', engine='xlsxwriter')
+    BD_sheets = {}
+    # len(SphereLink1)
+    if PARSNUM == 1:
+        ParsingVar = PARSCOUNT
+    else:
+        if PARSNUM == 2: ParsingVar = len(SphereLink1)
+    for i in range(ParsingVar):  # строка для запуска парсера  вставить строку свыше
+        ParsUrl = SphereLink1[i]
+        a = parsSphere(ParsUrl, 2)
+        BD_sheets[str(i + 1) + ' '] = a
+    for sheet_name in BD_sheets.keys():
+        BD_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
+    writer.close()
+else:
+    for i in range(1):
+        ParsUrl = SphereLink1[i]
+        a = parsSphere(ParsUrl, 1)
+        print(a)
+
+#
+# salaries1 = a
+# salaries2 = pd.DataFrame({'Name': ['K. De Bruyne', 'Neymar Jr', 'R. Lewandowski'],
+#                                         'Salary': [370000, 270000, 240000]})
+# salaries3 = pd.DataFrame({'Name': ['Alisson', 'M. ter Stegen', 'M. Salah'],
+#                                         'Salary': [160000, 260000, 250000]})
+# salary_sheets = {'Group1': salaries1, 'Group2': salaries2, 'Group3': salaries3}
+# writer = pd.ExcelWriter('./salaries.xlsx', engine='xlsxwriter')
+#
+# for sheet_name in salary_sheets.keys():
+#     salary_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
